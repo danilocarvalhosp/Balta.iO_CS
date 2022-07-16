c1ac52f53b1b5786504cbadc468615cf15bedc04
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-07-16 18:38:42 -0300

    GUID - Global Unique Identifier

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        // USANDO GUID: GLOBAL UNIQUE IDENTIFIER
+        static void Main(string [] args) {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("de7cc307-0f6a-494c-b3d8-73cff111cff3");            
+            Console.WriteLine(id.ToString().Substring(0, 8));            
+        }
+    }
+}
