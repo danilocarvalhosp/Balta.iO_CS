a96a4ec34708b2d466b800b7235a778cdcdde55e
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-07-16 19:13:36 -0300

    StringBuilder

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+using System.Text;
 
 namespace MyApp
 {
     internal class Program
     {
-        // EQUALS
-        static void Main(string [] args) { 
-            var texto = "Este texto é um teste";
-            Console.WriteLine(texto.Equals("Este texto é um teste"));
-            Console.WriteLine(texto.Equals("este texto é um teste"));
-            Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
+        // STRINGBUILDER
+        static void Main(string [] args) {
+            var texto = new StringBuilder();
+            texto.Append("Este texto é um teste ");
+            texto.Append("para guardar em memória ");
+            texto.Append("textos de várias linhas ");
+            texto.Append("dividindo-os em partes");
 
-            var teste = 33;
-            Console.WriteLine(teste.Equals(33));
-            ;
+            texto.ToString();
+            Console.WriteLine(texto);
         }
     }
 }
